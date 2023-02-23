580908f280029b4494ac0bc0b6cd1474bd70877c
Dump-Duck <nguyenhoainam28112003@gmail.com>
2023-02-24 01:08:29 +0700

    update data

--- NCKHSV/MainApp/urls.py
+++ NCKHSV/MainApp/urls.py
@@ -7,4 +7,5 @@ urlpatterns = [
     path('upload/', views.upload, name='upload'),
     path('manage/', views.manage, name='manage'),
     path('update/<int:id>/', views.update, name='update'),
+    # path('detail/<int:id>/', views.house_detail, name='detail'),
 ]
--- NCKHSV/MainApp/views.py
+++ NCKHSV/MainApp/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render, redirect
+from django.urls import resolve
 from django.http import HttpResponse
 from django.template import loader
 from .forms import *
@@ -66,50 +67,73 @@ def update(request, id):
     filter_province = provinces.objects.all()
     filter_district = districts.objects.all()
     filter_ward = wards.objects.all()
-    house_for_rent = houses_for_rent.objects.all()[id-1]
+    image_data = Image.objects.filter(houses=id)
+    house_for_rent = houses_for_rent.objects.prefetch_related('house_type', 'province', 'district', 'ward').get(id=id)
     if request.method == "POST":
-        updateForm = UploadForm(request.POST, request.FILES, instance=house_for_rent)
+        updateForm = UpdateForm(id, request.POST, request.FILES)
         if updateForm.is_valid():
-            house_type = house_types.objects.get(id=request.POST.get('house_type'))
-            address = request.POST.get('address')
-            province = provinces.objects.get(id=request.POST.get('province'))
-            district = districts.objects.get(id=request.POST.get('district'))
-            ward = wards.objects.get(id=request.POST.get('ward'))
-            price_per_month = request.POST.get('price_per_month')
-            area = request.POST.get('area')
-            images = request.FILES.getlist('images')
-            description = request.POST.get('description')
-            price_per_water_num = request.POST.get('price_per_water_num')
-            price_per_electric_num = request.POST.get('price_per_electric_num')
-            junk_money = request.POST.get('junk_money')
-            air_conditioner = request.POST.get('air_conditioner')
-            wardrobe = request.POST.get('wardrobe')
-            fan = request.POST.get('fan')
-            wc = request.POST.get('wc')
-            cooking_area = request.POST.get('cooking_area')
-            parking_area = request.POST.get('parking_area')
-            car_parking_area = request.POST.get('car_parking_area')
-            pet_allow = request.POST.get('pet_allow')
+            house_for_rent.house_type = house_types.objects.get(id=request.POST.get('house_type'))
+            house_for_rent.address = request.POST.get('address')
+            house_for_rent.province = provinces.objects.get(id=request.POST.get('province'))
+            house_for_rent.district = districts.objects.get(id=request.POST.get('district'))
+            house_for_rent.ward = wards.objects.get(id=request.POST.get('ward'))
+            house_for_rent.price_per_month = request.POST.get('price_per_month')
+            house_for_rent.area = request.POST.get('area')
+            images_upload = request.FILES.getlist('images')
+            house_for_rent.description = request.POST.get('description')
+            house_for_rent.price_per_water_num = request.POST.get('price_per_water_num')
+            house_for_rent.price_per_electric_num = request.POST.get('price_per_electric_num')
+            house_for_rent.junk_money = request.POST.get('junk_money')
+            house_for_rent.air_conditioner = request.POST.get('air_conditioner')
+            house_for_rent.wardrobe = request.POST.get('wardrobe')
+            house_for_rent.fan = request.POST.get('fan')
+            house_for_rent.wc = request.POST.get('wc')
+            house_for_rent.cooking_area = request.POST.get('cooking_area')
+            house_for_rent.parking_area = request.POST.get('parking_area')
+            house_for_rent.car_parking_area = request.POST.get('car_parking_area')
+            house_for_rent.pet_allow = request.POST.get('pet_allow')
+            # house_for_rent = id=id, house_type=house_type, address=address, province=province, district=district, ward=ward,
+            #                                             price_per_month=price_per_month, area=area, description=description, 
+            #                                             price_per_water_num=price_per_water_num, price_per_electric_num=price_per_electric_num,
+            #                                             junk_money=junk_money, air_conditioner=air_conditioner, wardrobe=wardrobe, fan=fan, wc=wc,
+            #                                             cooking_area=cooking_area, parking_area=parking_area, car_parking_area=car_parking_area,
+            #                                             pet_allow=pet_allow, coordinates=None
+            house_for_rent.save()
             
-            save_house = houses_for_rent.objects.create(house_type=house_type, address=address, province=province, district=district, ward=ward,
-                                                        price_per_month=price_per_month, area=area, description=description, 
-                                                        price_per_water_num=price_per_water_num, price_per_electric_num=price_per_electric_num,
-                                                        junk_money=junk_money, air_conditioner=air_conditioner, wardrobe=wardrobe, fan=fan, wc=wc,
-                                                        cooking_area=cooking_area, parking_area=parking_area, car_parking_area=car_parking_area,
-                                                        pet_allow=pet_allow, coordinates=None)
-            save_house.save()
-            
-            for image in images:
-                save_img = Image.objects.create(images=image, houses=save_house)
-                save_img.save()
+            for i in image_data:
+                for j in images_upload:
+                    i.images = j
+                    i.save()
             return redirect('/')
         else:
             print(updateForm.errors)
     else:
-        updateForm = UploadForm()
-    return render(request, 'update.html', {'updateForm': updateForm, 'house_types': filter_house, 'provinces': filter_province, 'districts': filter_district, 'wards': filter_ward})
+        updateForm = UpdateForm(id)
+    return render(request, 'update.html', {'updateForm': updateForm, 'house_types': filter_house, 'provinces': filter_province, 'districts': filter_district, 'wards': filter_ward, 'oldData': house_for_rent})
 
 # Manage all posts of Inn
 def manage(request):
     all_posts = houses_for_rent.objects.prefetch_related('house_type', 'province', 'district', 'ward').all()
     return render(request, 'manage.html', {'allPosts': all_posts})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+

864fe761dde204a2ab077eb732955fc36f4ba1a4
Cuong <Cuong@gmail.com>
2023-05-28 16:08:59 +0700

    add like

--- NCKHSV/MainApp/static/js/reply.js
+++ NCKHSV/MainApp/static/js/reply.js
@@ -17,8 +17,11 @@ document.addEventListener('DOMContentLoaded', function() {
     });
   });
 
-  
 
+
+
+  
+// Tạo đánh giá ngôi sao
   document.addEventListener('DOMContentLoaded', function() {
     // Mã JavaScript của bạn ở đây
  
@@ -79,4 +82,41 @@ document.addEventListener('DOMContentLoaded', function() {
   });
   
   ratingInfo.textContent = currentRating > 0 ? `Đánh giá: ${currentRating} sao` : 'Chưa đánh giá';
+});
+
+
+
+// Tạo lượt thích
+document.addEventListener('DOMContentLoaded', function() {
+  var likeButton = document.getElementById("likeButton");
+  var dislikeButton = document.getElementById("dislikeButton");
+  var likeCount = document.getElementById("likeCount");
+  var dislikeCount = document.getElementById("dislikeCount");
+  
+  // Kiểm tra xem có dữ liệu lưu trữ trong Local Storage hay không
+  var storedLikes = localStorage.getItem("likes");
+  var storedDislikes = localStorage.getItem("dislikes");
+  
+  // Khởi tạo số lượt thích và không thích từ dữ liệu lưu trữ (nếu có)
+  var like = storedLikes ? parseInt(storedLikes) : 0;
+  var dislike = storedDislikes ? parseInt(storedDislikes) : 0;
+  
+  // Cập nhật số lượt thích và không thích lên giao diện
+  likeCount.textContent = like;
+  dislikeCount.textContent = dislike;
+  
+  likeButton.addEventListener("click", function() {
+    like++;
+    likeCount.textContent = like;
+    // Lưu số lượt thích vào Local Storage
+    localStorage.setItem("likes", like.toString());
+  });
+  
+  dislikeButton.addEventListener("click", function() {
+    dislike++;
+    dislikeCount.textContent = dislike;
+    // Lưu số lượt không thích vào Local Storage
+    localStorage.setItem("dislikes", dislike.toString());
+  });
+  
 });
